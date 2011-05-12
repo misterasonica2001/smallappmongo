342d71d56a2123baa533f6e3a512669329a3b78d
misterasonica2001 <cosmin.ristea@hotmail.com>
2011-05-12 23:07:32 +0300

    12 mai 12

--- config/environment.rb
+++ config/environment.rb
@@ -2,9 +2,6 @@
 require File.expand_path('../application', __FILE__)
 #require 'my_date_override'
 
-Rails::Initializer.run do |config|
-config.gem "mongo_mapper"
-end
 # Initialize the rails application
 SmallApp::Application.initialize!
 
